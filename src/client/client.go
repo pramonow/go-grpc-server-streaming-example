c1b9c9bf9c87f4baaaf570a2e03e875aa602b831
Pramono <pramono_winata@yahoo.com>
2020-11-10 10:57:02 +0700

    initial commit

---
+++ src/client/client.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log"
+	"math/rand"
+
+	pb "github.com/pramonow/go-grpc-server-streaming/src/proto"
+
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	// dail server
+	conn, err := grpc.Dial(":50005", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("can not connect with server %v", err)
+	}
+
+	// create stream
+	client := pb.NewStreamServiceClient(conn)
+	in := &pb.Request{Id: 1}
+	stream, err := client.FetchResponse(context.Background(), in)
+	if err != nil {
+		log.Fatalf("openn stream error %v", err)
+	}
+
+	//ctx := stream.Context()
+	done := make(chan bool)
+
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err == io.EOF {
+				done <- true //close(done)
+				return
+			}
+			if err != nil {
+				log.Fatalf("can not receive %v", err)
+			}
+			log.Printf("Resp received: ", resp.Result)
+		}
+	}()
+
+	<-done
+	log.Printf("finished")
+}
---
+++ src/server/server.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	pb "github.com/pramonow/go-grpc-server-streaming/src/proto"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func (s server) FetchResponse(in *pb.Request, srv pb.StreamService_FetchResponseServer) error {
+
+	log.Printf("fetch response for id : %d", in.Id)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(count int64) {
+			defer wg.Done()
+			time.Sleep(time.Duration(count) * time.Second)
+			resp := pb.Response{Result: fmt.Sprintf("Request #%d For Id:%d", count, in.Id)}
+			if err := srv.Send(&resp); err != nil {
+				log.Printf("send error %v", err)
+			}
+			log.Printf("finishing request number : %d", count)
+		}(int64(i))
+	}
+
+	wg.Wait()
+	return nil
+}
+
+func main() {
+	// create listiner
+	lis, err := net.Listen("tcp", ":50005")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	// create grpc server
+	s := grpc.NewServer()
+	pb.RegisterStreamServiceServer(s, server{})
+
+	log.Println("start server")
+	// and start...
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
